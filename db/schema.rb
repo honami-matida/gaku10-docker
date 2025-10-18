1f4c7b35d703d0df3d63e7e0a08f1ca78921c6c5
町田穂波 <machidahonami@machidahonaminoMacBook-Air.local>
2025-10-18 10:10:03 +0900

    MySQL再構築

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2024_08_03_132148) do
 
-  create_table "active_storage_attachments", charset: "latin1", force: :cascade do |t|
+  create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2024_08_03_132148) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", charset: "latin1", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8mb4", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -34,13 +34,13 @@ ActiveRecord::Schema.define(version: 2024_08_03_132148) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", charset: "latin1", force: :cascade do |t|
+  create_table "active_storage_variant_records", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "admins", charset: "latin1", force: :cascade do |t|
+  create_table "admins", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2024_08_03_132148) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  create_table "customers", charset: "latin1", force: :cascade do |t|
+  create_table "customers", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -67,21 +67,21 @@ ActiveRecord::Schema.define(version: 2024_08_03_132148) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
-  create_table "favorites", charset: "latin1", force: :cascade do |t|
+  create_table "favorites", charset: "utf8mb4", force: :cascade do |t|
     t.integer "customer_id"
     t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "genres", charset: "latin1", force: :cascade do |t|
+  create_table "genres", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "group_requests", charset: "latin1", force: :cascade do |t|
+  create_table "group_requests", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "customer_id", null: false
     t.bigint "group_id", null: false
     t.integer "is_status", default: 0, null: false
@@ -91,14 +91,14 @@ ActiveRecord::Schema.define(version: 2024_08_03_132148) do
     t.index ["group_id"], name: "index_group_requests_on_group_id"
   end
 
-  create_table "group_users", charset: "latin1", force: :cascade do |t|
+  create_table "group_users", charset: "utf8mb4", force: :cascade do |t|
     t.integer "customer_id"
     t.integer "group_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "groups", charset: "latin1", force: :cascade do |t|
+  create_table "groups", charset: "utf8mb4", force: :cascade do |t|
     t.integer "genre_id"
     t.string "name"
     t.text "introduction"
@@ -108,7 +108,7 @@ ActiveRecord::Schema.define(version: 2024_08_03_132148) do
     t.index ["owner_id"], name: "index_groups_on_owner_id"
   end
 
-  create_table "notifications", charset: "latin1", force: :cascade do |t|
+  create_table "notifications", charset: "utf8mb4", force: :cascade do |t|
     t.string "subject_type"
     t.bigint "subject_id"
     t.bigint "customer_id"
@@ -120,7 +120,7 @@ ActiveRecord::Schema.define(version: 2024_08_03_132148) do
     t.index ["subject_type", "subject_id"], name: "index_notifications_on_subject"
   end
 
-  create_table "post_comments", charset: "latin1", force: :cascade do |t|
+  create_table "post_comments", charset: "utf8mb4", force: :cascade do |t|
     t.integer "post_id"
     t.integer "customer_id"
     t.text "comment"
@@ -128,7 +128,7 @@ ActiveRecord::Schema.define(version: 2024_08_03_132148) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "posts", charset: "latin1", force: :cascade do |t|
+  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.integer "customer_id"
     t.integer "genre_id"
     t.string "title"
@@ -137,7 +137,7 @@ ActiveRecord::Schema.define(version: 2024_08_03_132148) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "tags", charset: "latin1", force: :cascade do |t|
+  create_table "tags", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
