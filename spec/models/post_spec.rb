ae1a44536410fa6526b1d70cd7bb7da84383d7dd
honami-matida <yuzuyuzu202405@gmail.com>
2025-10-27 21:38:10 +0900

    Fix: set utf8mb4 encoding and define genre in post_spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:post) { build(:post, genre: genre) }
+  let(:customer) { create(:customer) }
+  let(:genre) { create(:genre) }
+  let(:post) { build(:post, customer: customer, genre: genre) }
 
   context 'バリデーションのテスト' do
     it 'タイトルと本文があれば有効である' do
